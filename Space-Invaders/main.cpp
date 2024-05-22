4b7545f54e544c2daad5110eee0dcb60eb9e38ba
TheOne41799 <anudeepramadugu@gmail.com>
2024-05-23 01:07:24 +0530

    First commit - creating a game window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,37 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    return 0;
+	sf::VideoMode videoMode = *(new sf::VideoMode(800, 800));
+
+	sf::RenderWindow* renderWindow = new sf::RenderWindow(videoMode, "SFML Game Window");
+
+	//sf::RenderWindow* renderWindow = new sf::RenderWindow(videoMode, "Resizable Window", sf::Style::Resize);
+
+	/*sf::RenderWindow* renderWindow = new sf::RenderWindow(sf::VideoMode::getFullscreenModes()[0],
+		"Full Screen Window", sf::Style::Fullscreen);*/
+
+	//renderWindow->setFramerateLimit(60);
+
+	//renderWindow->setPosition(sf::Vector2i(300, 300));
+
+
+	while (renderWindow->isOpen())
+	{
+		sf::Event event;
+
+		while (renderWindow->pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				renderWindow->close();
+			}
+		}		
+
+		renderWindow->clear(sf::Color(255,140,0));
+
+		//renderWindow->setPosition(sf::Vector2i(300, 300));
+
+		renderWindow->display();
+	}
 }
