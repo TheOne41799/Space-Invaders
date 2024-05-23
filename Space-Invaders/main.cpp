75005c6a87c9d607f8d23b074d1b0a28871c9054
TheOne41799 <anudeepramadugu@gmail.com>
2024-05-23 18:12:27 +0530

    Creating player data

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+using namespace std;
 
 
 class Player
 {
-    
-};
+private:
+    int playerHealth = 10;
+    sf::Vector2f playerPosition = sf::Vector2f(100, 100);
+    int playerMovementSpeed = 6;
+    int playerScore = 0;
+public:
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
 
+    int GetPlayerScore()
+    {
+        return playerScore;
+    }
+
+    void SetPlayerScore(int newScore)
+    {
+        playerScore = newScore;
+    }
+
+    void PlayerTakeDamage(){}
+    void PlayerMove(){}
+    void PlayerShootBullets(){}
+};
 
 
 int main()
@@ -21,6 +42,10 @@ int main()
 
     sf::VideoMode videoMode(800, 600);
     sf::RenderWindow renderWindow(videoMode, "SFML Window");
+
+    Player player;
+
+    std::cout << player.GetPlayerScore() << std::endl;
 
     while (renderWindow.isOpen())
     {
