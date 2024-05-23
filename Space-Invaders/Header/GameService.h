f5916cb55a3fe9e10eb7631f370d99e8931d852a
TheOne41799 <anudeepramadugu@gmail.com>
2024-05-23 22:14:31 +0530

    Create Game Service class

---
+++ Space-Invaders/Header/GameService.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class GameService
+{
+private:
+	void Initialize();
+	void Destroy();
+
+public:
+	GameService();
+	~GameService();
+
+	void Ignite();
+	void Update();
+	void Render();
+	bool IsRunning();
+};
+
+
