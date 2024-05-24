80e079e555ffabb8e641ccd4253ad5c0dd35af5f
TheOne41799 <anudeepramadugu@gmail.com>
2024-05-25 04:49:33 +0530

    Created the GameService class

---
+++ Space-Invaders/Header/GameService.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class GameService
+{
+private:
+	void Initialize();
+	void Destroy();
+
+public:
+	GameService();
+	~GameService();
+
+	void Ignite();
+	void Update();
+	void Render();
+	bool IsRunning();
+};
---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,43 @@
+#include "../Header/GameService.h"
+
+
+void GameService::Initialize()
+{
+	//Initialize objects
+}
+
+void GameService::Destroy()
+{
+	//Clean up of objects
+}
+
+GameService::GameService()
+{
+	//contructor
+}
+
+GameService::~GameService()
+{
+	//destructor
+}
+
+void GameService::Ignite()
+{
+	//start the game
+}
+
+void GameService::Update()
+{
+	//game loop
+}
+
+void GameService::Render()
+{
+	//show things on screen - render the next frame
+}
+
+bool GameService::IsRunning()
+{
+	//check if the game is running
+	return false;
+}
