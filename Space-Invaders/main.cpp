02d222873de376d2b2f9a2698957d12a07f2588b
TheOne41799 <anudeepramadugu@gmail.com>
2024-05-25 04:14:47 +0530

    Render Window and Player class

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,32 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+
+class Player
+{
+
+};
+
+
 
 int main()
 {
-    return 0;
-}
+    sf::VideoMode videoMode(800, 600);
+    sf::RenderWindow renderWindow(videoMode, "SFML Window");
+
+    while (renderWindow.isOpen())
+    {
+        sf::Event event;
+
+        while (renderWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                renderWindow.close();
+            }
+        }
+
+        renderWindow.clear(sf::Color::Blue);
+
+        renderWindow.display();
+    }
